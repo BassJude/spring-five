f72dc68667bf4593001f4c673883d91c220178d3
Aneta <adam.pierzchala.muzyk@gmail.com>
2020-01-08 21:54:20 +0100

    2

---
+++ src/main/java/com/pierzchala/springfive/controller/DesignTacoController.java
@@ -0,0 +1,41 @@
+package com.pierzchala.springfive.controller;
+
+import com.pierzchala.springfive.tacos.Ingredient;
+import com.pierzchala.springfive.tacos.Taco;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Slf4j
+@Controller
+@RequestMapping("/design")
+public class DesignTacoController {
+
+    @GetMapping
+    public String showDesignForm(Model model) {
+        List<Ingredient> ingredients = Arrays.asList(
+                new Ingredient("FLTO", "pszenna", Ingredient.Type.WRAP),
+                new Ingredient("COTO", "kukurydziana", Ingredient.Type.WRAP),
+                new Ingredient("GRBF", "mielona wołowina ", Ingredient.Type.PROTEIN),
+                new Ingredient("CARN", "kawałki mięsa", Ingredient.Type.PROTEIN),
+                new Ingredient("TMTO", "pomidory pokrojone w kostkę", Ingredient.Type.VEGGIES),
+                new Ingredient("LETC", "sałata", Ingredient.Type.VEGGIES),
+                new Ingredient("CHED", "cheddar", Ingredient.Type.CHEESE),
+                new Ingredient("JACK", "Monterey Jack", Ingredient.Type.CHEESE),
+                new Ingredient("SLSA", "pikantny sos pomidorowy", Ingredient.Type.SAUCE),
+                new Ingredient("SRCR", "śmietana", Ingredient.Type.SAUCE)
+        );
+        Ingredient.Type[] types = Ingredient.Type.values();
+        for (Ingredient.Type type : types) {
+//            model.addAttribute(type.toString().toLowerCase(),
+//            filterByType(ingredients, type));
+        }
+        model.addAttribute("design", new Taco());
+        return "design";
+    }
+}
---
+++ src/main/java/com/pierzchala/springfive/tacos/Taco.java
@@ -0,0 +1,4 @@
+package com.pierzchala.springfive.tacos;
+
+public class Taco {
+}
