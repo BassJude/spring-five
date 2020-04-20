ca4ff8e3e6c8c31f72a671640032ddf2d07dcd67
Aneta <adam.pierzchala.muzyk@gmail.com>
2020-04-20 23:01:10 +0200

    2004202301

--- src/test/java/com/pierzchala/springfive/HomeControllerTest.java
+++ src/test/java/com/pierzchala/springfive/HomeControllerTest.java
@@ -1,18 +1,18 @@
 package com.pierzchala.springfive;
 
 import com.pierzchala.springfive.controller.HomeController;
+import io.sentry.Sentry;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(HomeController.class)
@@ -20,6 +20,12 @@ public class HomeControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    JavaMailSender mailSender;
+
+//    @Autowired
+//    private MailService mailService;
 
     @Test
     public void testHomePage() throws Exception {
@@ -29,4 +35,19 @@ public class HomeControllerTest {
                 .andExpect(content().string(
                         containsString("Witaj w...")));
     }
+
+    @Test
+    public void sentryStart() {
+        Sentry.init("https://52c6d9c2d6b54968bb42fef5cc9ae5f0@sentry.io/5186900?environment=dev&stacktrace.app.packages=com.pierzchala");
+        Sentry.capture("This is a test");
+
+        System.out.println(Sentry.isInitialized());
+    }
+
+    @Test
+    public void sendMail() {
+//        sendMail("statek.kosmiczny@gmail.com","Jest Super", "Wiadomość",false);
+    }
+
+
 }
